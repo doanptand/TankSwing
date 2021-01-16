6aa52248f1c4c8c2d9cc8c9021e5c307707d671b
Trung Doan <doanpt93cntt@gmail.com>
2021-01-16 23:02:15 +0700

    add more intersect when move boss

--- src/com/ddona/tank/model/TankBoss.java
+++ src/com/ddona/tank/model/TankBoss.java
@@ -43,8 +43,8 @@ public class TankBoss extends Tank {
     }
 
     public void moveTank(MapManager mapManager, TankPlayer player, Bird bird, BossManager bossManager, boolean changeOrient) {
+        //FIXME fix thuat toan de impove khi boss va cham tuong. quay dau chua nhanh
         if (changeOrient) {
-            System.out.println("change orient");
             getNewOrient();
         }
         switch (this.orient) {
@@ -54,41 +54,100 @@ public class TankBoss extends Tank {
                     if (isIntersectWithMapAndBird(mapManager, bird)) {
                         y += speed;
                         getNewOrient();
-                        System.out.println("va cham map or bird");
                     }
+                } else if (isIntersectWithPlayer(player)) {
+                    y += speed;
+                    this.orient = Const.DOWN_ORIENT;
+                } else if (isIntersectWithOtherBoss(bossManager)) {
+                    y += speed;
+                    getNewOrient();
+                    System.out.println();
+                } else if (nearCollision(bossManager, player)) {
+                    y += speed;
+                    getNewOrient();
                 }
                 break;
             case Const.DOWN_ORIENT:
                 if (y < Const.MAP_SIZE - Const.TANK_SIZE) {
                     y += speed;
                     if (isIntersectWithMapAndBird(mapManager, bird)) {
-                        System.out.println("va cham map or bird");
                         y -= speed;
                         getNewOrient();
                     }
+                } else if (isIntersectWithPlayer(player)) {
+                    y -= speed;
+                    this.orient = Const.UP_ORIENT;
+                } else if (isIntersectWithOtherBoss(bossManager)) {
+                    y -= speed;
+                    getNewOrient();
+                } else if (nearCollision(bossManager, player)) {
+                    y -= speed;
+                    getNewOrient();
                 }
                 break;
             case Const.LEFT_ORIENT:
                 if (x > 0) {
                     x -= speed;
                     if (isIntersectWithMapAndBird(mapManager, bird)) {
-                        System.out.println("va cham map or bird");
                         x += speed;
                         getNewOrient();
                     }
+                } else if (isIntersectWithPlayer(player)) {
+                    x += speed;
+                    this.orient = Const.RIGH_ORIENT;
+                } else if (isIntersectWithOtherBoss(bossManager)) {
+                    x += speed;
+                    getNewOrient();
+                } else if (nearCollision(bossManager, player)) {
+                    x += speed;
+                    getNewOrient();
                 }
                 break;
             case Const.RIGH_ORIENT:
                 if (x < Const.MAP_SIZE - Const.TANK_SIZE) {
                     x += speed;
                     if (isIntersectWithMapAndBird(mapManager, bird)) {
-                        System.out.println("va cham map or bird");
                         x -= speed;
                         getNewOrient();
                     }
+                } else if (isIntersectWithPlayer(player)) {
+                    x -= speed;
+                    this.orient = Const.LEFT_ORIENT;
+                } else if (isIntersectWithOtherBoss(bossManager)) {
+                    x -= speed;
+                    getNewOrient();
+                } else if (nearCollision(bossManager, player)) {
+                    x -= speed;
+                    getNewOrient();
                 }
                 break;
         }
+    }
+
+    private boolean nearCollision(BossManager bossManager, TankPlayer player) {
+        //TODO implement more logic to check near Collision between tank-boss and boss-boss
+//        for (int i = 0; i < bossManager.getBosses().size(); i++) {
+//            if (!bossManager.getBosses().get(i).equals(this)) {
+//                if (bossManager.getBosses().get(i).getRect().union(getRect()).getHeight() < 111
+//                        || bossManager.getBosses().get(i).getRect().union(getRect()).getWidth() < 111) {
+//                    return true;
+//                }
+//            }
+//        }
+//        if (getRect().union(player.getRect()).getWidth() < 140 || getRect().union(player.getRect()).getHeight() < 140) {
+//            return true;
+//        }
+        return false;
+    }
+
+    private boolean isIntersectWithOtherBoss(BossManager bossManager) {
+        //TODO implement intersect with boss-boss
+        return false;
+    }
+
+    private boolean isIntersectWithPlayer(TankPlayer player) {
+        //TODO implement intersect between boss-tank
+        return false;
     }
 
     private boolean isIntersectWithMapAndBird(MapManager mapManager, Bird bird) {
