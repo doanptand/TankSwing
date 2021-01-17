fc5d2f3c6ae624aa8da8a9b76e4474f16dbcd25d
Trung Doan <doanptand@gmail.com>
2021-01-17 19:25:58 +0700

    do multiple actions at the same time

--- src/com/ddona/tank/gui/MapPanel.java
+++ src/com/ddona/tank/gui/MapPanel.java
@@ -11,12 +11,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.BitSet;
 
-public class MapPanel extends JPanel implements KeyListener {
+public class MapPanel extends JPanel implements KeyListener, Runnable {
     private MapManager mapManager;
     private Bird mBird;
     private TankPlayer mTankPlayer;
     private BulletManager mBulletManager;
+    private boolean isRunning = true;
+    private final BitSet mBitSet = new BitSet(256);
 
     public MapPanel() {
         setBounds((Const.WIDTH_FRAME - Const.MAP_SIZE) / 2,
@@ -31,7 +34,9 @@ public class MapPanel extends JPanel implements KeyListener {
     }
 
     private void initComponents() {
+        mBitSet.clear();
         mapManager = new MapManager(1);
+        isRunning = true;
         mBird = new Bird(
                 12 * Const.ITEM_SIZE,
                 24 * Const.ITEM_SIZE,
@@ -39,6 +44,7 @@ public class MapPanel extends JPanel implements KeyListener {
                 Const.TANK_SIZE);
         mTankPlayer = new TankPlayer();
         mBulletManager = new BulletManager();
+        new Thread(this).start();
     }
 
     @Override
@@ -58,32 +64,51 @@ public class MapPanel extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("key press:" + e.getKeyChar());
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-                mTankPlayer.moveTank(Const.UP_ORIENT);
-                break;
-            case KeyEvent.VK_DOWN:
-                mTankPlayer.moveTank(Const.DOWN_ORIENT);
-                break;
-            case KeyEvent.VK_LEFT:
-                mTankPlayer.moveTank(Const.LEFT_ORIENT);
-                break;
-            case KeyEvent.VK_RIGHT:
-                mTankPlayer.moveTank(Const.RIGH_ORIENT);
-                break;
-            case KeyEvent.VK_SPACE:
-                mBulletManager.addBullet(new Bullet(Const.TANK_ID,
-                        mTankPlayer.getX() + (Const.TANK_SIZE - Const.BULLET_SIZE) / 2
-                        , mTankPlayer.getY() + (Const.TANK_SIZE - Const.BULLET_SIZE) / 2,
-                        mTankPlayer.getOrient()));
-                break;
+        if (e.getKeyCode() == KeyEvent.VK_P) {
+            this.isRunning = !isRunning;
         }
-        repaint();
+        mBitSet.set(e.getKeyCode());
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        mBitSet.clear(e.getKeyCode());
+    }
 
+    @Override
+    public void run() {
+        while (true) {
+            if (isRunning && mBird.isAlive()) {
+                mBulletManager.moveAllBullets();
+            }
+            try {
+                Thread.sleep(7);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            catchEvent();
+            repaint();
+        }
+    }
+
+    private void catchEvent() {
+        if (!isRunning) {
+            return;
+        }
+        if (mBitSet.get(KeyEvent.VK_UP)) {
+            mTankPlayer.moveTank(Const.UP_ORIENT);
+        } else if (mBitSet.get(KeyEvent.VK_DOWN)) {
+            mTankPlayer.moveTank(Const.DOWN_ORIENT);
+        } else if (mBitSet.get(KeyEvent.VK_LEFT)) {
+            mTankPlayer.moveTank(Const.LEFT_ORIENT);
+        } else if (mBitSet.get(KeyEvent.VK_RIGHT)) {
+            mTankPlayer.moveTank(Const.RIGH_ORIENT);
+        }
+        if (mBitSet.get(KeyEvent.VK_SPACE)) {
+            mBulletManager.addBullet(new Bullet(Const.TANK_ID,
+                    mTankPlayer.getX() + (Const.TANK_SIZE - Const.BULLET_SIZE) / 2
+                    , mTankPlayer.getY() + (Const.TANK_SIZE - Const.BULLET_SIZE) / 2,
+                    mTankPlayer.getOrient()));
+        }
     }
 }
